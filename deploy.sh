10f367fe69d910153f5c147d79d69eb5062d4f74
Ross Riley <riley.ross@gmail.com>
2013-09-24 13:08:32 +0100

    add jekyll build to shell file

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/collecta/deployer; /usr/bin/git fetch --all; /usr/bin/git reset --hard origin/master;
+cd /home/collecta/deployer; /usr/bin/git fetch --all; /usr/bin/git reset --hard origin/master; jekyll build -d ../site/
