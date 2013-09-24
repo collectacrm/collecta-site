8ea30b3eef40382353411bfeac121ab7155ded4c
Ross Riley <riley.ross@gmail.com>
2013-09-24 14:17:41 +0100

    full paths for jekyll build

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/collecta/deployer; /usr/bin/git fetch --all; /usr/bin/git reset --hard origin/master; /usr/local/rvm/gems/ruby-2.0.0-p247/bin/jekyll build -d ../site/
+cd /home/collecta/deployer; /usr/bin/git fetch --all; /usr/bin/git reset --hard origin/master; /usr/local/rvm/gems/ruby-2.0.0-p247/bin/jekyll build -s /home/collecta/deployer -d /home/collecta/site
