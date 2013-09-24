28c79383cdaf39a1ff7d86b24050e501d5921862
Ross Riley <riley.ross@gmail.com>
2013-09-24 12:02:39 +0100

    force exec to dev/null

--- github-receive.php
+++ github-receive.php
@@ -12,13 +12,11 @@ $github_cidrs = array('204.232.175.64/27', '192.30.252.0/22');
  
 if(cidr_match($_SERVER['REMOTE_ADDR'], $github_cidrs)) {
   error_log("[WEBHOOK] Received github request as ".date("d j Y H:i:s"));
-  exec("git fetch --all");
-  exec("git reset --hard origin/master");
+  exec("git fetch --all 2>&1");
+  exec("git reset --hard origin/master 2>&1");
   echo 'Done.';
 } else {
-  error_log("[WEBHOOK / FAIL] Failed github request as ".date("d j Y H:i:s"));
-  //header('HTTP/1.1 404 Not Found');
-  //echo '404 Not Found.';
-  echo exec("git status");
+  header('HTTP/1.1 404 Not Found');
+  echo '404 Not Found.';
   exit;
 }
