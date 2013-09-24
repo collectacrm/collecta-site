6f36c18ce10835ef1ac0b63ed4e216f516de0d4f
Ross Riley <riley.ross@gmail.com>
2013-09-24 11:54:45 +0100

    debugging

--- github-receive.php
+++ github-receive.php
@@ -11,10 +11,12 @@ function cidr_match($ip, $ranges) {
 $github_cidrs = array('204.232.175.64/27', '192.30.252.0/22');
  
 if(cidr_match($_SERVER['REMOTE_ADDR'], $github_cidrs)) {
+  error_log("[WEBHOOK] Received github request as ".date("d j Y H:i:s"));
   exec("git fetch --all");
   exec("git reset --hard origin/master");
   echo 'Done.';
 } else {
+  error_log("[WEBHOOK / FAIL] Failed github request as ".date("d j Y H:i:s"));
   //header('HTTP/1.1 404 Not Found');
   //echo '404 Not Found.';
   echo exec("git status");
