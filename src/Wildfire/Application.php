2642465b6bba921db0c7b14ec6b22ad62638777f
Charles Marshall <charles@oneblackbear.com>
2013-11-07 17:02:58 +0000

    twitter share link

--- src/Wildfire/Application.php
+++ src/Wildfire/Application.php
@@ -9,9 +9,10 @@ class Application extends Wax {
 
   public function route($request) {
     parent::route($request);
+    $this->server = "http://".$_SERVER['HTTP_HOST'];
     \WaxUrl::map("", array("controller"=>"Wildfire\ Cms","action"=>"index"));
     \WaxUrl::map(":action", array("controller"=>"Wildfire\ Cms"));
     \WaxUrl::map(":action/:id", array("controller"=>"Wildfire\ Cms"));
   }
-  
+
 }
