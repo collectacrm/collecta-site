e8655fe10ef8facfbe1c7e87d9d9e065173fe107
Charles Marshall <charles@oneblackbear.com>
2013-11-11 16:54:26 +0000

    updated version with a customisable column

--- app/model/CContent.php
+++ app/model/CContent.php
@@ -6,5 +6,8 @@ class CContent extends WildfireContent{
     if(($children = $this->children) && ($children = $children->scope("live")->limit($limit)->all()) && $children->count() ) return $children;
     return false;
   }
+  public function format_content($col="content") {
+    return str_replace("__t__", "", CmsTextFilter::filter("before_output", $this->$col) );
+  }
 }
 ?>
